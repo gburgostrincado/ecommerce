95114e95fa8fda08cd864037f804274753a41f85
Gonzalo Burgos <gburgostrincado@gmail.com>
2025-05-11 22:32:29 -0400

    delete client to separate

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const port = process.env.PORT || 5001;
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(cors());
 app.use(express.json());
 
@@ -27,14 +24,6 @@ db.sequelize.authenticate()
   .catch(err => {
     console.error('❌ Unable to connect to the database:', err);
   });
-
-if (process.env.NODE_ENV === 'production') {
-  const clientBuildPath = path.resolve(__dirname, 'client', 'build');
-  app.use(express.static(clientBuildPath));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(clientBuildPath, 'index.html'));
-  });
-}
 
 app.listen(port, () => {
   console.log(`✅ Server is running on port ${port}`);
